b3de99962394e2a60e8684276619e1dc3858a526
RitishGoel16 <ritishgoel2002@gmail.com>
2024-07-30 16:22:52 +0530

    add div

---
+++ src/main/webapp/resources/js/script.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+ document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    });
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", () => {
+            hamburger.classList.remove("active");
+            navMenu.classList.remove("active");
+        });
+    });
+});
+
+
+ 
+
+
